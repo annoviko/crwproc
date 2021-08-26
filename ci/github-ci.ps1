3048d8f14419dca6ec169e74a16d9d717c2d65e5
annoviko <spb.andr@yandex.ru>
2021-08-26 15:27:27 +0200

    #21: Corrent name of the solution.

--- ci/github-ci.ps1
+++ ci/github-ci.ps1
@@ -1,9 +1,9 @@
 New-Variable -Name JobBuild -Value "test-crwproc-build" -Option ReadOnly -Force
-
+New-Variable -Name SolutionPath -Value crwproc.sln -Option ReadOnly -Force
 
 
 function Build-Application($Configuration) {
-	msbuild ccore\ccore.sln /t:crwproc /p:configuration=$Configuration /p:platform=x64
+	msbuild $SolutionPath /t:crwproc /p:configuration=$Configuration /p:platform=x64
 	if ($LastExitCode -ne 0) {
 		Write-Error "Building crwproc failed with error code '$LastExitCode'."
 		Exit 1
