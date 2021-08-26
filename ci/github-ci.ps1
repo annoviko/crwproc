840d82e78b92567f07f61994f5ae40fc85b7345d
annoviko <spb.andr@yandex.ru>
2021-08-26 15:25:06 +0200

    #21: CI script to build crwproc.

---
+++ ci/github-ci.ps1
@@ -0,0 +1,33 @@
+New-Variable -Name JobBuild -Value "test-crwproc-build" -Option ReadOnly -Force
+
+
+
+function Build-Application($Configuration) {
+	msbuild ccore\ccore.sln /t:crwproc /p:configuration=$Configuration /p:platform=x64
+	if ($LastExitCode -ne 0) {
+		Write-Error "Building crwproc failed with error code '$LastExitCode'."
+		Exit 1
+	}
+}
+
+
+function Run-BuildTestJob {
+	Build-Application "Release"
+}
+
+
+function Run-ContinousIntegration($Job) {
+	switch($Job) {
+		$JobBuild { 
+			Run-BuildTestJob
+			break
+		}
+		default {
+			Write-Error "Unknown job is specified '$Job'." -TargetObject $Job
+			Exit 1
+		}
+	}
+}
+
+
+Run-ContinousIntegration $args[0]
