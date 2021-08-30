dc9efe48b9949d15ed38f9206e96bb58912e3dd6
annoviko <spb.andr@yandex.ru>
2021-08-30 09:28:26 +0200

    [ut] Add more unit-tests.

--- tests/ut/ut-value.cpp
+++ tests/ut/ut-value.cpp
@@ -62,3 +62,50 @@ TEST(ut_value, create_float_from_buffer) {
 TEST(ut_value, create_double_from_buffer) {
     test_template_value_from_buffer(double(6.022));
 }
+
+
+TEST(ut_value, create_int8_from_string) {
+    test_template_value_from_string("0", std::int8_t(0));
+    test_template_value_from_string("-100", std::int8_t(-100));
+    test_template_value_from_string("120", std::int8_t(120));
+}
+
+
+TEST(ut_value, create_int16_from_string) {
+    test_template_value_from_string("32760", std::int16_t(32760));
+    test_template_value_from_string("-32760", std::int16_t(-32760));
+}
+
+
+TEST(ut_value, create_int32_from_string) {
+    test_template_value_from_string("536870912", std::int32_t(536870912));
+    test_template_value_from_string("-536870912", std::int32_t(-536870912));
+}
+
+
+TEST(ut_value, create_int64_from_string) {
+    test_template_value_from_string("72057594037927936", std::int64_t(72057594037927936));
+    test_template_value_from_string("-72057594037927936", std::int64_t(-72057594037927936));
+}
+
+
+TEST(ut_value, create_uint8_from_string) {
+    test_template_value_from_string("0", std::uint8_t(0));
+    test_template_value_from_string("127", std::uint8_t(127));
+    test_template_value_from_string("254", std::uint8_t(254));
+}
+
+
+TEST(ut_value, create_uint16_from_string) {
+    test_template_value_from_string("65535", std::uint16_t(65535));
+}
+
+
+TEST(ut_value, create_uint32_from_string) {
+    test_template_value_from_string("2147483648", std::uint32_t(2147483648));
+}
+
+
+TEST(ut_value, create_uint64_from_string) {
+    test_template_value_from_string("68719476736", std::uint64_t(68719476736));
+}
--- tests/ut/ut-value.h
+++ tests/ut/ut-value.h
@@ -25,3 +25,26 @@ test_template_value_from_buffer(TypeValue p_value) {
     ASSERT_EQ(p_value, val.get<TypeValue>());
     ASSERT_EQ(std::to_string(p_value), val.get<std::string>());
 }
+
+
+template <typename TypeValue>
+typename std::enable_if<std::is_fundamental<TypeValue>::value, void>::type
+test_template_value_from_string(const std::string& p_str_value, const TypeValue p_value) {
+    value val;
+
+    if constexpr (std::is_signed<TypeValue>::value) {
+        val = value(value::get_type<TypeValue>(), sizeof(TypeValue), true, p_str_value);
+    }
+    else if constexpr (std::is_unsigned<TypeValue>::value) {
+        val = value(value::get_type<TypeValue>(), sizeof(TypeValue), false, p_str_value);
+    }
+    else {
+        static_assert(false);
+    }
+
+    ASSERT_TRUE(val.is_valid());
+    ASSERT_EQ(sizeof(TypeValue), val.get_size());
+    ASSERT_EQ(value::get_type<TypeValue>(), val.get_type());
+    ASSERT_EQ(p_value, val.get<TypeValue>());
+    ASSERT_EQ(p_str_value, val.get<std::string>());
+}
