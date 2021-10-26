5cb1df6d9324501363f68d39ba0d094f86b4969f
annoviko <spb.andr@yandex.ru>
2021-10-26 11:01:37 +0200

    #33: Download, install and run cppcheck in order to check sources.

--- ci/github-ci.ps1
+++ ci/github-ci.ps1
@@ -9,9 +9,41 @@ $ProjectUtName = "ut.vcxproj"
 $BinaryUtPath = "$SolutionPath\x64\Release"
 $BinaryUtName = "ut.exe"
 
+$CppCheckUrlInstaller = "https://github.com/danmar/cppcheck/releases/download/2.6/cppcheck-2.6-x64-Setup.msi"
+$CppCheckInstaller = "cppcheck-2.6-x64-Setup.msi"
+$CppCheckPath = "C:\CppCheck"
+$CppCheckApplication = "cppcheck.exe"
+
 
 function Announce-Step($Message) {
     Write-Host "[New Step] $Message" -ForegroundColor green
+}
+
+
+function Download-CppCheck() {
+    Announce-Step "Download cppcheck for Windows from github release page."
+
+    $WebClient = New-Object System.Net.WebClient
+
+    $Attempts = 3
+    for($i = 0; $i -lt $Attempts; $i++){
+        try {
+            $WebClient.DownloadFile($CppCheckUrlInstaller, $CppCheckInstaller)
+            break
+        }
+        Catch [Exception]{
+            Write-Host "[Info] CppCheck downloading failed (attempts: '$i')." -ForegroundColor Green
+            Start-Sleep 1
+        }
+    }
+    
+    if (Test-Path $CppCheckInstaller) {
+        Write-Host "[Info] CppCheck installer file has been download to '$CppCheckInstaller'." -ForegroundColor Green
+    }
+    else {
+        Write-Error -Message "[Error] CppCheck installer has not been downloaded." -Category ResourceUnavailable
+        Exit 1
+    }
 }
 
 
@@ -19,6 +51,41 @@ function Install-PythonPackages() {
     Announce-Step "Install Python Packages."
     
     pip install robotframework robotframework-httpctrl
+}
+
+
+function Install-CppCheck() {
+    Announce-Step "Install CppCheck."
+    
+    Download-CppCheck
+    
+    $InstallArguments = "/InstallationType=AllUsers /S /AddToPath=1 D=$CppCheckPath"
+    
+    Start-Process -FilePath $CppCheckInstaller -ArgumentList $InstallArguments -Wait -Passthru
+
+    if (Test-Path $CppCheckPath) {
+        Write-Host "[Info] CppCheck has been successfully installed to '$CppCheckPath'." -ForegroundColor Green
+        Remove-Item $CppCheckInstaller
+    }
+    else {
+        Write-Error -Message "[Info] CppCheck has not been installed to '$CppCheckPath' with error code: '$LastExitCode'."
+        Exit 1
+    }
+    
+    $env:PATH = "$env:PATH;$CppCheckPath"
+}
+
+
+function Run-CppCheck() {
+    Announce-Step "Analyse Application Source Code."
+    
+    Install-CppCheck
+    
+    $CppCheckApplication --inline-suppr --error-exitcode=1 --std=c++17 --inconclusive --enable=warning,style,performance,information,portability core control log crwproc
+    if ($LastExitCode -ne 0) {
+        Write-Error "[Error] Analysis crwproc failed with error code '$LastExitCode'."
+        Exit 1
+    }
 }
 
 
@@ -60,6 +127,8 @@ function Build-SubjectApplication {
 function Run-UnitTests {
     Announce-Step "Run Unit-Tests."
     
+    Build-UnitTests
+    
     & "$BinaryUtPath\$BinaryUtName"
     
     if ($LastExitCode -ne 0) {
@@ -92,8 +161,8 @@ function Run-BuildTestJob {
     
     Build-Application "Release"
     
-    Build-UnitTests
     Run-UnitTests
+    Run-CppCheck
     Run-SctTests
 }
 
